3d0171a1b892bb0ac834d3885e0b53b87c5c3d93
Tom Banish <tombanish@gmail.com>
2019-11-26 01:01:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,3 @@
-array = ["Tim", "Tom", "Jim"]
-
 def hello_t(array)
   i = 0
   
@@ -10,4 +8,8 @@ def hello_t(array)
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
